c0213be49378eb7b7da0118cc4d4572b0279af2b
Sayyid A Naqvi <naqvi.sdsu@gmail.com>
2023-10-25 20:09:19 -0500

    added dependencies, views for 3 fragments, improved kotlin Practice.kt

--- kotlinreview/src/main/java/ghar/learn/kotlinreview/Practice.kt
+++ kotlinreview/src/main/java/ghar/learn/kotlinreview/Practice.kt
@@ -6,20 +6,35 @@ fun main(){
     val zan = Person()
     zan.name = "Zain"
 
+    println("${zan.name} has eaten? ${zan.hasEaten}")
     zan.feedMe("Nashta")
+    println("${zan.name} has eaten? ${zan.hasEaten}")
     println("${zan.name} has consumed ${zan.calories} calories")
 }
 class Person {
-    var name = ""
+    /**
+     * Using setter() to validate the parameter or value passed in while
+     * it does not have 'it's own' backing-field, but using another property's one
+      */
+    var name : String = ""
+        set(value) {
+            if(value.isEmpty()) throw Exception("please enter a name, it can't be empty")
+            field = value
+        }
+
     var calories = 0
 
+    // Using backing field
+    val hasEaten : Boolean
+        get() {
+            return calories != 0            // return false if Person has not eaten anything, i.e: calories would be '0' then
+        }
+
     fun feedMe(food : String){
-        println("$name has been feed $food")
         updateCalories(20)
     }
 
-    fun updateCalories(calories : Int){
-//        calories += 10
+    fun updateCalories(calories : Int) {
         this.calories += calories     // 'this' is added to make explicit call to 'property of the class (that has same name)
     }
 
