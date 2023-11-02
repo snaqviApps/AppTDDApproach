95edca0ef3f31a7c858ab0972f01a97f8b014211
Sayyid A Naqvi <naqvi.sdsu@gmail.com>
2023-11-02 12:44:58 -0500

    application-class not found error, App crashes

--- app/src/main/java/ghar/learn/cognizant/apptddapproach/TddApproachApplication.kt
+++ app/src/main/java/ghar/learn/cognizant/apptddapproach/TddApproachApplication.kt
@@ -1,8 +1,5 @@
 package ghar.learn.cognizant.apptddapproach
 import android.app.Application
-import android.content.Context
-import androidx.multidex.MultiDex
-import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
